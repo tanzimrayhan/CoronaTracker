ec9b809001cb764d5be4ec351ea3d757e74913c8
tanzimrayhan <tanzim_rayhan@yahoo.com>
2020-05-10 22:29:56 -0400

    Put the choose option in top

--- src/App.js
+++ src/App.js
@@ -32,8 +32,9 @@ class App extends React.Component {
     const { data, country } = this.state;
     return (
       <div className={styles.container}>
-        <Cards data={data} />
         <CountryChooser handleCountryChange={this.handleCountryChange} />
+
+        <Cards data={data} />
         <Chart data={data} country={country} />
 
       </div>
